477f64511c11b417e35529e1d0aed63d8797c7cb
Carson Vayhinger <vayhingercc@email.wofford.edu>
2019-10-14 13:54:31 -0400

    Added Creation of database

--- src/main/java/edu/wofford/wocoin/main/Feature01Main.java
+++ src/main/java/edu/wofford/wocoin/main/Feature01Main.java
@@ -1,6 +1,18 @@
 package edu.wofford.wocoin.main;
 
+import edu.wofford.wocoin.Utilities;
+import java.util.Scanner;
+
 public class Feature01Main {
     //main file for Feature01
-
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter Database Name: ");
+            String databaseName = in.next();
+            Utilities.createNewDatabase(databaseName);
+        } else {
+            System.out.println("You must specify the database filepath as a command-line argument.");
+        }
+    }
 }
