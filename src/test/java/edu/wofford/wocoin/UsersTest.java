1eee9f91d822e02bc7ca0751f455a8af418811b3
Avery McMillen <averymcmillen@gmail.com>
2019-10-15 16:48:25 -0400

    Set Up UsersTest.java pt 2

--- src/test/java/edu/wofford/wocoin/UsersTest.java
+++ src/test/java/edu/wofford/wocoin/UsersTest.java
@@ -6,8 +6,14 @@ import org.junit.Before;
 public class UsersTest {
 
     @Test
-    public void UsersTest(){
+    public void UsersTest1(){
+        //first test should be to determine if database exists
 
+    }
+
+    @Test
+    public void UsersTest2(){
+        //second test on if user exists / can be created
     }
 }
 
