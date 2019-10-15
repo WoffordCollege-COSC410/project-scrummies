08c3684553d97bc01eb8c4cef563b78a64294e81
Avery McMillen <averymcmillen@gmail.com>
2019-10-15 16:56:00 -0400

    Database created and populated

--- src/main/java/edu/wofford/wocoin/main/Feature01Main.java
+++ src/main/java/edu/wofford/wocoin/main/Feature01Main.java
@@ -7,10 +7,7 @@ public class Feature01Main {
     //main file for Feature01
     public static void main(String[] args) {
         if (args.length > 0) {
-            Scanner in = new Scanner(System.in);
-            System.out.println("Enter Database Name: ");
-            String databaseName = in.nextLine();
-            Utilities.createNewDatabase(databaseName);
+            Utilities.createNewDatabase(args[0]);
         } else {
             System.out.println("You must specify the database filepath as a command-line argument.");
         }
