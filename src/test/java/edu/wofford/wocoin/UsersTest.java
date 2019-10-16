6a4c14b3c9d54720867aaebe867e282a25bbea12
Avery McMillen <averymcmillen@gmail.com>
2019-10-16 16:22:34 -0400

    Progress - sort of

--- src/test/java/edu/wofford/wocoin/UsersTest.java
+++ src/test/java/edu/wofford/wocoin/UsersTest.java
@@ -1,7 +1,13 @@
 package edu.wofford.wocoin;
 import static org.junit.Assert.*;
+
+import ch.qos.logback.core.db.DriverManagerConnectionSource;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Before;
+
+import java.sql.DriverAction;
+import java.sql.DriverManager;
 
 public class UsersTest {
 
@@ -9,6 +15,8 @@ public class UsersTest {
     public void UsersTest1(){
         //first test should be to determine if database exists
 
+        //DriverManager da = new DriverAction();
+        //String actualString = da.toString();
     }
 
     @Test
