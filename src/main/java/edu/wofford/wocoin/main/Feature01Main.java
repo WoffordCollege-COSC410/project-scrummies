0c6403ca8cb6f4578bfa93766ca4473adccbaaff
ledfordls <ledfordls@email.wofford.ediu>
2019-10-17 17:16:14 -0600

    Feature01 Updates

--- src/main/java/edu/wofford/wocoin/main/Feature01Main.java
+++ src/main/java/edu/wofford/wocoin/main/Feature01Main.java
@@ -2,6 +2,7 @@ package edu.wofford.wocoin.main;
 
 import edu.wofford.wocoin.Users;
 import edu.wofford.wocoin.Utilities;
+import org.web3j.abi.datatypes.Int;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,58 +10,65 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 import java.io.File;
+import java.sql.PreparedStatement;
 
 public class Feature01Main {
     //main file for Feature01
     public static void main(String[] args) {
         boolean Still_running = true;
         if (args.length > 0) {
-            while (Still_running == true) {
-                String url = "jdbc:sqlite:" + args[0];
+            String url = "jdbc:sqlite:" + args[0];
 
-                File f = new File(args[0]);
+            File f = new File(args[0]);
 
-                Scanner input = new Scanner(System.in);
-                System.out.println("1: exit\n2: Admin\n");
-                String answer = input.nextLine();
-                if (answer.equals("1")) {
-                    // this exits out of the program
-                    Still_running = false;
-                } else if (answer.equals("2")) {
-                    System.out.println("1: back\n2: add user\n");
-                    String next_answer = input.nextLine();
-                    if (next_answer.equals("1")) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("1: exit\n2: Admin\n");
+            String answer = input.nextLine();
+            if (answer.equals("1")) {
+                // this exits out of the program
+            } else if (answer.equals("2")) {
+                System.out.println("1: back\n2: add user\n");
+                String next_answer = input.nextLine();
+                if (next_answer.equals("1")) {
 
-                    } else if (next_answer.equals("2")) {
-                        System.out.println("Enter a user");
-                        String user = input.nextLine();
-                        String password = input.nextLine();
-                        Users.AddUser(user, password);
-                        String[] sqls = {"INSERT INTO users (id, salt, hash) VALUES (\"\", 13587, \"ebd3528832b124bb7886cd8e8d42871c99e06d5f3ad0c6ee883f6219b2b6a955\")"};
-                        if (f.exists()) {
-                            try (Connection conn = DriverManager.getConnection(url)) {
-                                for (String sql : sqls) {
-                                    Statement stmt = conn.createStatement();
-                                    stmt.executeUpdate(sql);
-                                    stmt.close();
-                                    // Wait for one second so that timestamps are different.
-                                    try {
-                                        Thread.sleep(1000);
-                                    } catch (InterruptedException e) {
-                                        Thread.currentThread().interrupt();
-                                    }
+                } else if (next_answer.equals("2")) {
+                    System.out.println("Enter a user");
+                    String user = input.nextLine();
+                    String password = input.nextLine();
+                    int int_salt = Utilities.generateSalt();
+                    String salt = Integer.toString(int_salt);
+                    String hash = "" + Utilities.applySha256(password) + salt;
+                    String sqls = "INSERT INTO users (id, salt, hash) VALUES (?, ?, ?)";
+                    prepStmt = conn.prepareStatement(sqlStmt);
+                    prepStmt.setString(1, "user");
+                    prepStmt.setString(2, "salt");
+                    prepStmt.setString(3, "hash");
+
+
+                    if (f.exists()) {
+                        try (Connection conn = DriverManager.getConnection(url)) {
+                            for (String sql : sqls) {
+                                Statement stmt = conn.createStatement();
+                                stmt.executeUpdate(sql);
+                                stmt.close();
+                                // Wait for one second so that timestamps are different.
+                                try {
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException e) {
+                                    Thread.currentThread().interrupt();
                                 }
-                            } catch (SQLException e) {
-                                e.printStackTrace();
                             }
-                        } else {
-                            Utilities.createNewDatabase(args[0]);
+                        } catch (SQLException e) {
+                            e.printStackTrace();
                         }
                     } else {
-                        System.out.println("You must specify the database filepath as a command-line argument.");
+                        Utilities.createNewDatabase(args[0]);
                     }
+                } else {
+                    System.out.println("You must specify the database filepath as a command-line argument.");
                 }
             }
+
 
         }
     }
