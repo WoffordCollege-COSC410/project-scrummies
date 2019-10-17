533bd97a874a2017818479d4f4320212d09a6823
Avery McMillen <averymcmillen@gmail.com>
2019-10-17 16:36:14 -0400

    UsersTest updates

--- src/test/java/edu/wofford/wocoin/UsersTest.java
+++ src/test/java/edu/wofford/wocoin/UsersTest.java
@@ -11,12 +11,25 @@ public class UsersTest {
     public void UsersTest1(){
         //first test should be to determine if database exists
         //String databaseName = Feature01Main;
+        Users tester = new Users();
 
     }
 
     @Test
     public void UsersTest2(){
-        //second test on if user exists / can be created
+        //second test is see if admin info is correct
+        Users tester = new Users();
+        assertEquals(tester.AddUser("jdoe", 13587), "username is: \npassword is:");
+        assertEquals(tester.AddUser("jsmith", 52196), "username is: \npassword is:");
+        assertEquals(tester.AddUser("hjones", 47440), "username is: \npassword is:");
+        assertEquals(tester.AddUser("srogers", 54419), "username is: \npassword is:");
+    }
+
+    @Test
+    public void UsersTest3(){
+        //third test on if user exists / can be created
+        Users tester = new Users();
+
     }
 
 }
