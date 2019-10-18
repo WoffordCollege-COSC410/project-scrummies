2f9762746c4ffbfb34d4725c175f49a9d9683e36
Avery McMillen <averymcmillen@gmail.com>
2019-10-18 14:02:21 -0400

    Updated Commented Out file

--- src/test/java/edu/wofford/wocoin/UsersTest.java
+++ src/test/java/edu/wofford/wocoin/UsersTest.java
@@ -16,27 +16,5 @@ public class UsersTest {
         Feature01Main tester = new Feature01Main();
 
     }
-    /*
-    @Test
-    public void InputTest() {
-        String[] Salt_Hash = Users.SaltPassword("test");
-        int int_salt = Integer.parseInt(Salt_Hash[0]);
-        String hash = Salt_Hash[1];
-        try (Connection conn = DriverManager.getConnection(url)) {
-            Statement stmt = conn.createStatement();
-            String sqls = "INSERT INTO users (id, salt, hash) VALUES (?, ?, ?)";
-            PreparedStatement prepStmt = conn.prepareStatement(sqls);
-            prepStmt.setString(1, id);
-            prepStmt.setInt(2, int_salt);
-            System.out.println("" + id + " " + int_salt + " " + hash);
-            prepStmt.setString(3, hash);
-            prepStmt.executeUpdate();
-
-            prepStmt.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-    */
 }
 
