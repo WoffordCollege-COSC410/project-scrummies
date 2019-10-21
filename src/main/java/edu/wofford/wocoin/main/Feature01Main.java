ccd96b05af23948faa613361084f3c02280f404f
ledfordls <ledfordls@email.wofford.ediu>
2019-10-21 13:41:52 -0600

    Some progress

--- src/main/java/edu/wofford/wocoin/main/Feature01Main.java
+++ src/main/java/edu/wofford/wocoin/main/Feature01Main.java
@@ -11,3 +11,15 @@ import java.sql.Statement;
 import java.util.Scanner;
 import java.io.File;
 import java.sql.PreparedStatement;
+
+public class Feature01Main {
+    //main file for Feature01
+    public static void main(String[] args) {
+        boolean Still_running = true;
+        if (args.length > 0) {
+            while (Still_running) {
+
+            }
+        }
+    }
+}
