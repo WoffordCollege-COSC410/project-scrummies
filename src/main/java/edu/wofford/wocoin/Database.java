6e78588ff567364b94ad804ad86989f0cbe88a30
Avery McMillen <averymcmillen@gmail.com>
2019-10-23 15:36:32 -0400

    Re-added lost lines

--- src/main/java/edu/wofford/wocoin/Database.java
+++ src/main/java/edu/wofford/wocoin/Database.java
@@ -2,6 +2,7 @@ package edu.wofford.wocoin;
 import gherkin.lexer.Fi;
 
 import java.io.File;
+import java.sql.*;
 
 
 public class Database {
@@ -17,7 +18,28 @@ public class Database {
         }
         return false;
     }
-    public void AddUser(String username, String password) {
+    public void AddUser(String filename, String id){
+        File file = new File(filename);
+        Database db= new Database("filename");
+        int salt = Utilities.generateSalt();
+
+        String hash = Utilities.applySha256(id + salt);
+
+        try (Connection conn = DriverManager.getConnection(filename)) {
+            Statement stmt = conn.createStatement();
+            String sqls = "INSERT INTO users (id, salt, hash) VALUES (?, ?, ?)";
+            PreparedStatement prepStmt = conn.prepareStatement(sqls);
+            prepStmt = conn.prepareStatement(sqls);
+            prepStmt.setString(1, id);
+            prepStmt.setInt(2, salt);
+            System.out.println("" + id + " " + salt + " " + hash);
+            prepStmt.setString(3, hash);
+            prepStmt.executeUpdate();
+
+            prepStmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
