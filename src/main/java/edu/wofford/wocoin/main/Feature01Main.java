7dd1efbc19713a27bd121d61bf9c6f6d9fcc7aff
Avery McMillen <averymcmillen@gmail.com>
2019-10-28 13:34:04 -0400

    Refactoring - removing Users

--- src/main/java/edu/wofford/wocoin/main/Feature01Main.java
+++ src/main/java/edu/wofford/wocoin/main/Feature01Main.java
@@ -1,17 +1,8 @@
 package edu.wofford.wocoin.main;
 
 import edu.wofford.wocoin.Database;
-import edu.wofford.wocoin.Users;
-import edu.wofford.wocoin.Utilities;
-import org.web3j.abi.datatypes.Int;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
-import java.io.File;
-import java.sql.PreparedStatement;
 
 public class Feature01Main {
     //main file for Feature01
