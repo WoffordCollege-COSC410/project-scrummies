a3d1748cd5a6a76983c924db0ae757438d822c89
Avery McMillen <averymcmillen@gmail.com>
2019-11-06 13:42:15 -0500

    Wallet class

--- src/main/java/edu/wofford/wocoin/Wallet.java
+++ src/main/java/edu/wofford/wocoin/Wallet.java
@@ -1,29 +1,23 @@
 package edu.wofford.wocoin;
 
 import java.io.File;
-import java.sql.*;
-import java.sql.PreparedStatement;
 import java.io.IOException;
 
+import org.web3j.crypto.CipherException;
+import org.web3j.crypto.Credentials;
 import org.web3j.crypto.WalletUtils;
-import org.web3j.protocol.Web3j;
-import org.web3j.protocol.http.HttpService;
-import org.web3j.protocol.core.methods.response.EthBlockNumber;
-import org.web3j.protocol.core.methods.response.EthGasPrice;
-import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 
 public class Wallet {
 
-    public void Create() {
-        File keystoreWallet = new File("idStore");
+    public void createWallet() {
+        File idStoreWallet = new File("idStore");
         String passwordWallet = "walletpwd";
         try {
-            String walletFile = WalletUtils.generateNewWalletFile(passwordWallet, keystoreWallet, false);
+            String walletFile = WalletUtils.generateNewWalletFile(passwordWallet, idStoreWallet, false);
             System.out.println("File name ethereum wallet: " + walletFile);
         } catch (Exception ex) {
             System.out.println(ex);
         }
-
-
     }
+
 }
