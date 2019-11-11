d4328d4e9ce388bf3e003ba6438f4841cb030725
ledfordls <ledfordls@email.wofford.ediu>
2019-11-11 12:56:34 -0700

    update on wallet

--- src/main/java/edu/wofford/wocoin/Wallet.java
+++ src/main/java/edu/wofford/wocoin/Wallet.java
@@ -9,12 +9,18 @@ import org.web3j.crypto.WalletUtils;
 
 public class Wallet {
 
+    private File file;
+
+    public Wallet(String walletName) {
+        file = new File(walletName);
+    }
+
     public void createWallet() {
-        File useridWallet = new File("keystore");
+        String home = System.getProperty("user.home");
+        file = new File(home + File.separator + "project-scrummies" + File.separator + "ethereum" + File.separator + "node0" + File.separator + "keystore");
         String passwordWallet = "walletpwd";
-        String walletPath = "UTC--2019-08-07T17-24-10.532680697Z--0fce4741f3f54fbffb97837b4ddaa8f769ba0f91.json";
         try {
-            String walletFile = WalletUtils.generateNewWalletFile(passwordWallet, useridWallet, false);
+            String walletFile = WalletUtils.generateNewWalletFile(passwordWallet, file, false);
             System.out.println("File name ethereum wallet: " + walletFile);
         } catch (Exception ex) {
             System.out.println(ex);
@@ -22,16 +28,17 @@ public class Wallet {
     }
 
 
-    public void getCredentials(){
+    public void getCredentials() {
 
         Credentials credentials;
 
-        File walletFile = new File ("keystore/your_wallet_file.json");
+        File walletFile = new File("C:\\Users\\sethl\\project-scrummies\\ethereum\\node0\\keystore\\UTC--2019-11-11T17-04-54.869000000Z--202d79c70190a105a7320c956f73ae3cbda2703c.json");
 
-        String passwordWallet="walletpwd";
+        String passwordWallet = "Seth";
 
         try {
             credentials = WalletUtils.loadCredentials(passwordWallet, walletFile);
+            System.out.println(credentials);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (CipherException e) {
